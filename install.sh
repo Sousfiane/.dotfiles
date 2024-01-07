05ef937705f1edc18136617bd71b15173bacec69
Sousfiane <thibaultcroisier@live.com>
2024-01-07 16:10:01 +0100

    first true test

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo install config
-ln -s ~/.dotfiles/.p10k.zsh ~/ 
-ln -s ~/.dotfiles/.zshrc ~/
-ln -s ~/.dotfiles/.gitconfig ~/
-ln -s ~/.dotfiles/.wallpapers ~/.wallpapers
-ln -s ~/.dotfiles/.fonts ~/.local/share/fonts
-ln -s ~/.dotfiles/.config/* ~/.config/
+ln -s ~/.dotfiles/p10k.zsh ~/.p10k.zsh
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/wallpapers ~/.wallpapers
+ln -s ~/.dotfiles/fonts ~/.local/share/fonts
+ln -s ~/.dotfiles/config/* ~/.config/
 
 
