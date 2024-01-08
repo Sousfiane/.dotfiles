3b26df0620800da04ecf2d65fa6847254d6b4b2c
Sousfiane <thibaultcroisier@live.com>
2024-01-08 07:53:12 +0100

    some deletions

--- config/Code - OSS/User/History/22c85a4b/jTTz.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo install config
-ln -s ~/.dotfiles/.p10k.zsh ~/ 
-ln -s ~/.dotfiles/.zshrc ~/
-ln -s ~/.dotfiles/.gitconfig ~/ 
-ln -s ~/.dotfiles/.config/* ~/.config/
-ln -s ~/.dotfiles/.gtk-3.0/* ~/.config/gtk-3.0/
-
