658b5b3fa0218e4b54b628b57bbdc4a05e688e34
Sousfiane <thibaultcroisier@live.com>
2024-01-11 10:46:28 +0100

    few changes

--- config/polybar/scripts/bluetooth/bluetooth.sh
+++ config/polybar/scripts/bluetooth/bluetooth.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#6e6a86}"
+  echo "%{F#6e6a86} "
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 31 ]
   then 
-    echo "%{F#C4A7E7}"
+    echo "%{F#C4A7E7} "
   fi
-  echo "%{F#f6c177}"
+  echo "%{F#f6c177} "
 fi
 
