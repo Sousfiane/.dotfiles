49ea1c57a7473f70bd62084e0f62e03c454bcbbb
Sousfiane <thibaultcroisier@live.com>
2024-06-02 15:57:15 +0200

    Auto update Sun Jun  2 03:57:15 PM CEST 2024

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1435,3 +1435,8 @@ c
 cd .dotfiles
 git init
 git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Sousfiane/.dotfiles.git
+git push -u origin main
+reboot
