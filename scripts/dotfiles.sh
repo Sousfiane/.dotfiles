9130ddd8afe0b116585b50d88d46e1e6993e5a73
Sousfiane <thibaultcroisier@live.com>
2024-06-05 10:15:09 +0200

    Wed Jun  5 10:15:09 AM CEST 2024

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -33,21 +33,21 @@ status(){
 }
 
 auto_pull(){
-    git -C $HOME/.dotfiles stash 2>&1
-    git -C $HOME/.dotfiles pull --rebase 2>&1
-    git -C $HOME/.dotfiles stash pop  2>&1
+    git -C $HOME/.dotfiles stash 2>/dev/null
+    git -C $HOME/.dotfiles pull --rebase 2>/dev/null
+    git -C $HOME/.dotfiles stash pop 2>/dev/null
 }
 
 auto_push(){
     auto_pull
-    git -C $HOME/.dotfiles add .  2>&1
-    git -C $HOME/.dotfiles commit --allow-empty -m "$(date)"  2>&1
-    git -C $HOME/.dotfiles push  2>&1
+    git -C $HOME/.dotfiles add .  2>/dev/null
+    git -C $HOME/.dotfiles commit --allow-empty -m "$(date)"  2>/dev/null
+    git -C $HOME/.dotfiles push  2>/dev/null
 }
 
 sync(){
     wait_for_internet
-    git -C $HOME/.dotfiles fetch  2>&1
+    git -C $HOME/.dotfiles fetch  2>/dev/null
     status=$(git -C $HOME/.dotfiles status --porcelain=v1)
     if [[ -n "$status" ]]; then
         auto_push
