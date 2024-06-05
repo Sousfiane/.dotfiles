11cebc1882a235f9ebc3241fb9dbd1ee89668272
Sousfiane <thibaultcroisier@live.com>
2024-06-05 09:12:05 +0200

    Wed Jun  5 09:12:05 AM CEST 2024

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -19,20 +19,12 @@ wait_for_internet(){
     done
 }
 
-
-cd_dotfiles(){
-    cd "$HOME/.dotfiles" || exit
-}
-
 notify(){
-    cd_dotfiles
-    notify-send "$(git status -s)"
+    notify-send "$(git -C $HOME/.dotfiles status -s)"
 }
 
 status(){
-    cd_dotfiles
-    index=$(git status -s | wc -l)
-
+    index=$(git -C $HOME/.dotfiles status -s | wc -l)
     if [ $index = 0 ]; then
         echo $index
     else
@@ -40,66 +32,23 @@ status(){
     fi
 }
 
-push(){
-    cd_dotfiles
-
-    actions(){
-        info "Here are the files in the index : "
-        git status -s | sed 's/^/        /'
-        user "Do you want to push those files ? (Y/n)"
-        read answer
-        case $answer in
-            [nN]* ) info "Exiting" ;
-                    exit ;;
-            * )     git add . ;
-                    git commit -m "Auto update $(date)" ;
-                    git push -u origin main ;
-                    success "Files has been pushed successfully !" ;;
-        esac
-    }
-
-    actions
-    sleep 2
-}
-
-pull(){
-    cd_dotfiles
-
-    actions(){
-        user "Do you want to pull from remote ? (Y/n)"
-        read answer
-        case $answer in
-            [nN]* ) info "Exiting" ;
-                    exit ;;
-            * )     git stash ;
-                    git pull ;
-                    success "Files has been updated" ;;
-        esac
-    }
-
-    actions
-    sleep 2
-}
-
-
 auto_pull(){
-    git -C $DOTFILES stash
-    git -C $DOTFILES  pull --rebase
-    git -C $DOTFILES stash pop
+    git -C $HOME/.dotfiles stash
+    git -C $HOME/.dotfiles  pull --rebase
+    git -C $HOME/.dotfiles stash pop
 }
 
 auto_push(){
     auto_pull
-    git -C $DOTFILES add .
-    echo 'lol'
-    git -C $DOTFILES commit --allow-empty -m "$(date)"
-    git -C $DOTFILES push
+    git -C $HOME/.dotfiles add .
+    git -C $HOME/.dotfiles commit --allow-empty -m "$(date)"
+    git -C $HOME/.dotfiles push
 }
 
 sync(){
     wait_for_internet
-    git -C $DOTFILES fetch
-    status=$(git -C $DOTFILES status --porcelain=v1)
+    git -C $HOME/.dotfiles fetch
+    status=$(git -C $HOME/.dotfiles status --porcelain=v1)
     if [[ -n "$status" ]]; then
         auto_push
     else
@@ -109,7 +58,5 @@ sync(){
 
 [[ $1 == "status" ]] && status
 [[ $1 == "notify" ]] && notify
-[[ $1 == "push" ]] && push 
-[[ $1 == "pull" ]] && pull
 [[ $1 == "sync" ]] && sync
 
