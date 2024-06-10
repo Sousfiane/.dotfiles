8bda4b72aa502016ff3f0eb375b67a4c7e1777d1
Sousfiane <thibaultcroisier@live.com>
2024-06-10 12:24:25 +0200

    Auto update : Mon Jun 10 12:24:25 PM CEST 2024

--- scripts/videos.sh
+++ scripts/videos.sh
@@ -4,11 +4,11 @@ dir="$HOME/.config/rofi/launcher"
 theme='style-1'
 videos="$HOME/Videos"
 
-while [ -z $file ]
+while [ -z "$file" ]
 do
-    folder=$(ls $videos | rofi -dmenu -i -theme $dir/$theme)
-    [ -z $folder ] || file=$(ls $videos/$folder |  rofi -dmenu -i -theme $dir/$theme)
-    [ -z $folder ] && [ -z $pdf ] && exit
+    folder="$(ls "$videos" | rofi -dmenu -i -theme "$dir/$theme")"
+    [ -z "$folder" ] || file=$(ls "$videos/$folder" |  rofi -dmenu -i -theme "$dir/$theme")
+    [ -z "$folder" ] && [ -z "$file" ] && exit
 done
 
-mpv --fs=yes $videos/$folder/$file
+mpv --fs=yes "$videos/$folder/$file"
