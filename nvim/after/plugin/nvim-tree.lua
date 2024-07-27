ea0f2dfe268091f0f5cfd99908bfbdbcff093a72
Sousfiane <thibaultcroisier@live.com>
2024-07-27 18:00:17 +0200

    Auto update : Sat Jul 27 06:00:17 PM CEST 2024

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,4 +1,7 @@
 require("nvim-tree").setup()
 local api = require("nvim-tree.api")
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.keymap.set("n", "<leader>pv", function() api.tree.toggle({ focus = true, find_file = true }) end)
