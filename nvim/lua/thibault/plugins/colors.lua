b69d7e663192e90231d5721072781d17ddaca3c0
Sousfiane <thibaultcroisier@live.com>
2024-07-28 17:02:37 +0200

    Auto update : Sun Jul 28 05:02:37 PM CEST 2024

--- nvim/lua/thibault/plugins/colors.lua
+++ nvim/lua/thibault/plugins/colors.lua
@@ -50,7 +50,7 @@ return {
             highlight_groups = {
 
                 ColorColumn = { bg = 'rose' },
-                CursorColumn ={ bg = 'highlight_med'},
+                CursorColumn ={bg = 'highlight_med' },
 
                 CursorLine = { bg = 'foam', blend = 10 },
                 StatusLine = { fg = 'love', bg = 'love', blend = 10 },
