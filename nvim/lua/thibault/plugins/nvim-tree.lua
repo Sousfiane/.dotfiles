a3fb4a9c084c7dbc87e4b9a61408d8117d4f2037
Sousfiane <thibaultcroisier@live.com>
2024-07-28 16:29:43 +0200

    Auto update : Sun Jul 28 04:29:43 PM CEST 2024

--- nvim/lua/thibault/plugins/nvim-tree.lua
+++ nvim/lua/thibault/plugins/nvim-tree.lua
@@ -2,7 +2,24 @@ return{
     'nvim-tree/nvim-tree.lua',
     dependencies = {'nvim-tree/nvim-web-devicons'},
     config = function()
-        require("nvim-tree").setup()
+        require("nvim-tree").setup({
+            view = {
+                adaptive_size = true,
+                centralize_selection = true,
+            },
+            update_focused_file = {
+                enable = true,
+            },
+            diagnostics = {
+                enable = true,
+                show_on_dirs = true,
+            },
+            actions = {
+                change_dir = {
+                    enable = false,
+                },
+            },
+        })
         local api = require("nvim-tree.api")
 
         vim.g.loaded_netrw = 1
