3f5e5a0d2ed7268a8902266ada008a6b0c83de4f
Sousfiane <thibaultcroisier@live.com>
2024-07-29 11:59:16 +0200

    Auto update : Mon Jul 29 11:59:16 AM CEST 2024

--- nvim/lua/thibault/plugins/harpoon.lua
+++ nvim/lua/thibault/plugins/harpoon.lua
@@ -1,7 +1,7 @@
-return{
+return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
-    dependencies = {'nvim-lua/plenary.nvim'},
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local harpoon = require("harpoon")
 
@@ -10,7 +10,9 @@ return{
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
     end
 }
