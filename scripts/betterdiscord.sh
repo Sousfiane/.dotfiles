79ec9be489763ae95ed7270272a0d41a3482cd4d
Sousfiane <thibaultcroisier@live.com>
2024-07-31 00:00:44 +0200

    Auto update : Wed Jul 31 12:00:44 AM CEST 2024

--- scripts/betterdiscord.sh
+++ scripts/betterdiscord.sh
@@ -2,5 +2,8 @@
 
 status=$(betterdiscordctl status | grep "injected" | cut -d':' -f 2)
 
-[[ $status == "no" ]] && echo lol && betterdiscordctl install && killall discord && discord &
+echo $status
+if [[ $status == "yes" ]]; then
+    echo "yes"
+fi
 
