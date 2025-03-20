9dc1ef5cd6343f9c0a465ce4439dc4e90009d732
Soufiane <thibaultcroisier@live.com>
2025-03-20 23:24:47 +0100

    Auto update : Thu Mar 20 11:24:47 PM CET 2025

--- nvim/lua/thibault/plugins/telescope.lua
+++ nvim/lua/thibault/plugins/telescope.lua
@@ -7,10 +7,6 @@ return {
 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
         vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
