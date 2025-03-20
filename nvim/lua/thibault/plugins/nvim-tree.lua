a95c3b7989ae4e97889448098c36d0e66deef954
Soufiane <thibaultcroisier@live.com>
2025-03-20 09:21:05 +0100

    Auto update : Thu Mar 20 09:21:05 AM CET 2025

--- nvim/lua/thibault/plugins/nvim-tree.lua
+++ nvim/lua/thibault/plugins/nvim-tree.lua
@@ -1,33 +1,46 @@
-return{
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {'nvim-tree/nvim-web-devicons'},
-    config = function()
-        require("nvim-tree").setup({
+return {
+  'nvim-tree/nvim-tree.lua',
+  dependencies = {'nvim-tree/nvim-web-devicons'},
+  config = function()
+    -- Set up NvimTree configuration
+    require("nvim-tree").setup({
+      view = {
+        side = "left",
+        adaptive_size = true,
+        centralize_selection = true,
+      },
+      update_focused_file = {
+        enable = true,
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+      },
+      actions = {
+        change_dir = {
+          enable = false,
+        },
+      },
+    })
 
-            view = {
-                side = "left",
-                adaptive_size = true,
-                centralize_selection = true,
-            },
-            update_focused_file = {
-                enable = true,
-            },
-            diagnostics = {
-                enable = true,
-                show_on_dirs = true,
-            },
-            actions = {
-                change_dir = {
-                    enable = false,
-                },
-            },
-        })
+    local api = require("nvim-tree.api")
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-        local api = require("nvim-tree.api")
+    -- Keymap to toggle NvimTree, but close UndoTree if it's open
+    vim.keymap.set("n", "<leader>pv", function()
+      -- Check if Undotree is open by looking for the Undotree buffer
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_get_name(buf):match("undotree") then
+          -- Close Undotree if it's open
+          vim.cmd('UndotreeHide')
+          break
+        end
+      end
 
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-
-        vim.keymap.set("n", "<leader>pv", function() api.tree.toggle({ focus = true, find_file = true }) end)
-    end
+      -- Toggle NvimTree
+      api.tree.toggle({ focus = true, find_file = true })
+    end)
+  end
 }
+
--- nvim/lua/thibault/plugins/undotree.lua
+++ nvim/lua/thibault/plugins/undotree.lua
@@ -1,7 +1,19 @@
-return{
-    'mbbill/undotree',
-    config = function ()
-        vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-        vim.g.undotree_SplitWidth = 40
-    end
+return {
+  'mbbill/undotree',
+  config = function()
+    vim.keymap.set('n', '<leader>u', function()
+      -- Check if NvimTree is open and close it
+      local nvim_tree_api = require("nvim-tree.api")
+      if nvim_tree_api.tree.is_visible() then
+        -- Close NvimTree
+        nvim_tree_api.tree.close()
+      end
+
+      -- Toggle UndoTree
+      vim.cmd.UndotreeToggle()
+    end)
+
+    -- Set Undotree width
+    vim.g.undotree_SplitWidth = 40
+  end
 }
