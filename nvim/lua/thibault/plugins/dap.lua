5593adf64884f402c2952caa4cd92a8436b364c7
Soufiane <thibaultcroisier@live.com>
2025-03-22 11:50:31 +0100

    Auto update : Sat Mar 22 11:50:31 AM CET 2025

--- nvim/lua/thibault/plugins/dap.lua
+++ nvim/lua/thibault/plugins/dap.lua
@@ -1,86 +1,79 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "theHamsta/nvim-dap-virtual-text",
-        "nvim-neotest/nvim-nio",
-    },
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+  },
+  keys = function(_, keys)
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+    return {
+      { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
+      { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
+      { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
+      { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
+      { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+      {
+        '<leader>B',
+        function()
+          dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = 'Debug: Set Breakpoint',
+      },
+      { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
+      unpack(keys),
+    }
+  end,
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
 
-        dap.set_log_level("TRACE")
+    require('mason-nvim-dap').setup {
+      automatic_installation = true,
+      handlers = {},
+      ensure_installed = {
+        'delve',
+      },
+    }
 
-        require("nvim-dap-virtual-text").setup()
+    dapui.setup {
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    }
 
-        dapui.setup({
-            layout = {
-                {
-                    elements = {
-                        { id = "scopes",      size = 0.25 },
-                        { id = "breakpoints", size = 0.25 },
-                        { id = "stacks",      size = 0.25 },
-                        { id = "watches",     size = 0.25 },
-                    },
-                    size = 12,
-                    position = "left",
-                },
-                {
-                    elements = {
-                        { id = "console", size = 0.5 },
-                        { id = "repl",    size = 0.5 },
-                    },
-                    size = 12,
-                    position = "bottom",
-                },
-            },
-            expand_lines = true,
-        })
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
+    dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    -- 💀 Make sure to update this path to point to your installation
+    args = {os.getenv('HOME') .."/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js", "${port}"},
+  }
+}
+dap.adapters.chrome = {
+    type = "executable",
+    command = "node",
+    args = {os.getenv("HOME") .. "/.local/share/nvim/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js"} -- TODO adjust
+}
 
-        dap.adapters["pwa-node"] = {
-            type = "server",
-            host = "localhost",
-            port = "${port}",
-            executable = {
-                command = "node",
-                args = { os.getenv("HOME") .. "/.config/dap/js-debug/src/dapDebugServer.js", "${port}" },
-            },
-        }
-
-        dap.adapters.chrome = {
-            type = "executable",
-            command = "node",
-            args = { os.getenv("HOME") .. "/.config/dap/vscode-chrome-debug/out/src/chromeDebug.js" },
-        }
-
-        local map = function(mode, lhs, rhs, opts)
-            opts = opts or {}
-            opts.silent = true
-            vim.keymap.set(mode, lhs, rhs, opts)
-        end
-
-        map("n", "<F5>", dap.continue, { desc = "Start/Continue Debugging" })
-        map("n", "<F10>", dap.step_over, { desc = "Step Over" })
-        map("n", "<F11>", dap.step_into, { desc = "Step Into" })
-        map("n", "<F12>", dap.step_out, { desc = "Step Out" })
-        map("n", "<Leader>b", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-        map("n", "<Leader>B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
-            { desc = "Set Conditional Breakpoint" })
-        map("n", "<Leader>dl", dap.run_last, { desc = "Run Last Debugging Session" })
-        map("n", "<Leader>du", function()
-            vim.cmd(":Neotree filesystem close")
-            vim.cmd('UndotreeHide')
-            dapui.toggle()
-        end, { desc = "Run Last Debugging Session" })
-    end,
+  end,
 }
