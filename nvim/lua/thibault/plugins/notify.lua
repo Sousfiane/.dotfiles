dc86025233f0947ef7b2db1e0de5960ffbd94d93
Soufiane <thibaultcroisier@live.com>
2025-03-23 10:41:00 +0100

    Auto update : Sun Mar 23 10:41:00 AM CET 2025

---
+++ nvim/lua/thibault/plugins/notify.lua
@@ -0,0 +1,14 @@
+return {
+	"rcarriga/nvim-notify",
+	config = function()
+		require("notify").setup({
+			stages = "fade_in_slide_out",
+			timeout = 3000,
+			background_colour = "#000000",
+			render = "default",
+			max_width = nil,
+			max_height = nil,
+		})
+		vim.notify = require("notify")
+	end,
+}
