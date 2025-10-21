73e8d58780fc22fba2ee4ca1b36319538f4c5b7d
Sousfiane <thibaultcroisier@live.com>
2025-10-21 23:02:56 +0200

    better package listing

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-updates=$(checkupdates)
-pkg_count=$(echo $updates | wc -w)
-pkg_list=$(echo $updates)
+updates=$(checkupdates | awk '{print $1}')
+
+pkg_count=0
+pkg_list="System up to date"
+
+if [[ -n "$updates" ]]; then
+    pkg_count=$(echo "$updates" | wc -l)
+    pkg_list="${updates//$'\n'/\\n}"
+fi
 
 echo "{\"text\":\"$pkg_count\", \"tooltip\":\"$pkg_list\"}"
+
