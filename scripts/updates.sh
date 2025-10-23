fec7d4031d4784bd1af40f98c61fea9f51d166f6
Sousfiane <thibaultcroisier@live.com>
2025-10-23 18:52:25 +0200

    better package listing

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 updates=$(checkupdates | awk '{print $1}')
-
 pkg_count=0
 pkg_list="System up to date"
 
 if [[ -n "$updates" ]]; then
     pkg_count=$(echo "$updates" | wc -l)
-    pkg_list="${updates//$'\n'/\\n}"
+    if (( pkg_count > 10 )); then
+        shown=$(echo "$updates" | head -n 10)
+        pkg_list="${shown//$'\n'/\\n}\\n..."
+    else
+        pkg_list="${updates//$'\n'/\\n}"
+    fi
 fi
 
 echo "{\"text\":\"$pkg_count\", \"tooltip\":\"$pkg_list\"}"
