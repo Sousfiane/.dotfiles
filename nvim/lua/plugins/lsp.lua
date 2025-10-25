2c7c5764c89cc6bf3aa2d0a7e63327c70bc5200f
Sousfiane <thibaultcroisier@live.com>
2025-10-25 19:53:43 +0200

    refactor lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,41 +1,40 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "mason-org/mason.nvim", opts = {} },
-		{
-			"mason-org/mason-lspconfig.nvim",
-			opts = { ensure_installed = { "lua_ls", "ts_ls", "html", "cssls", "clangd", "eslint" } },
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		opts = {
+			ensure_installed = { "lua_ls", "ts_ls", "html", "cssls", "clangd", "eslint" },
 		},
 	},
-	config = function()
-		vim.lsp.config("*", {
-			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		})
-
-		vim.lsp.config("lua_ls", {
-			settings = {
-				Lua = {
-					diagnostics = { globals = { "vim" } },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp", "mason-org/mason-lspconfig.nvim" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.config("*", { capabilities = capabilities })
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+					},
 				},
-			},
-		})
-
-		vim.diagnostic.config({
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = "",
-					[vim.diagnostic.severity.WARN] = "",
-					[vim.diagnostic.severity.INFO] = "",
-					[vim.diagnostic.severity.HINT] = "",
+			})
+			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "",
+						[vim.diagnostic.severity.WARN] = "",
+						[vim.diagnostic.severity.INFO] = "",
+						[vim.diagnostic.severity.HINT] = "",
+					},
 				},
-			},
-			float = {
-				header = "",
-				prefix = "",
-				suffix = "",
-			},
-			update_in_insert = true,
-		})
-	end,
+				float = { header = "", prefix = "", suffix = "" },
+				update_in_insert = true,
+			})
+		end,
+	},
 }
