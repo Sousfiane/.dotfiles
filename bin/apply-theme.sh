4dc2c0ecd985d5851ca5af8a1537e0a63b3472ea
Sousfiane <thibaultcroisier@live.com>
2025-11-09 17:32:49 +0100

    changed few utils

--- bin/apply-theme.sh
+++ bin/apply-theme.sh
@@ -9,5 +9,4 @@ gsettings set $GNOME color-scheme "light-dark"
 if [[ ! -f $HOME/.config/gtk-4.0/gtk.css ]]; then
     mkdir -p $HOME/.config/gtk-4.0
     ln -s /usr/share/themes/rose-pine-gtk/gtk-4.0/gtk.css $HOME/.config/gtk-4.0
-    echo mdr
 fi
