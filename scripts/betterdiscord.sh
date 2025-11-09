8e846824b82a8d516e4dde84375e1dc26320fdee
Sousfiane <thibaultcroisier@live.com>
2025-11-09 15:00:02 +0100

    removed script dir

--- scripts/betterdiscord.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-status=$(betterdiscordctl status | grep "injected" | cut -d':' -f 2)
-
-echo $status
-if [[ $status == "yes" ]]; then
-    echo "yes"
-fi
-
--- scripts/wallpapers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wallpapers_dir="$HOME/.wallpapers"
-
-wallpaper=$(ls $wallpapers_dir| sed '/links.prop/d' | wofi -S dmenu -aI -p Wallpapers)
-
-[ -z $wallpaper ] || (echo "$wallpapers_dir/$wallpaper" > $HOME/.swaybg && pkill swaybg && swaybg -i "$(cat ~/.swaybg)" &)
