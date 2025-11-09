69da33b6cf3d71cb7b20900cc37778e4cb97d166
Sousfiane <thibaultcroisier@live.com>
2025-11-09 19:58:13 +0100

    removed uneeded options

--- bin/wallpapers.sh
+++ bin/wallpapers.sh
@@ -2,6 +2,6 @@
 
 wallpapers_dir="$HOME/.wallpapers"
 
-wallpaper=$(ls $wallpapers_dir| sed '/links.prop/d' | wofi -S dmenu -aI -p Wallpapers)
+wallpaper=$(ls $wallpapers_dir| sed '/links.prop/d' | wofi -S dmenu -p Wallpapers)
 
 [ -z $wallpaper ] || (echo "$wallpapers_dir/$wallpaper" > $HOME/.swaybg && pkill swaybg && uwsm-app -- swaybg -i "$(cat ~/.swaybg)" &)
