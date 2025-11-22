bb3606f81a5f6506041a8da0df1da73f37e46960
Sousfiane <thibaultcroisier@live.com>
2025-11-22 22:49:46 +0100

    refactor wallpapers.sh

--- bin/wallpapers.sh
+++ bin/wallpapers.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
-wallpapers_dir="$HOME/.wallpapers"
+WALLPAPERS="$HOME/.wallpapers"
 
-wallpaper=$(ls $wallpapers_dir| sed '/links.prop/d' | wofi -S dmenu -p Wallpapers --cache-file=/dev/null)
+selected=$(ls $WALLPAPERS| sed '/links.prop/d' | wofi -S dmenu -p Wallpapers --cache-file=/dev/null)
 
-[[ -z "$wallpaper" ]] && exit 0
+[[ -z "$selected" ]] && exit 0
 
-echo "$wallpapers_dir/$wallpaper" > $HOME/.swaybg
+echo "$WALLPAPERS/$selected" > $HOME/.swaybg
 pkill swaybg
 setsid uwsm-app -- swaybg -i "$(cat ~/.swaybg)" >/dev/null 2>&1 &
